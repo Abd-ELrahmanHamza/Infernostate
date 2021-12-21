6f80169611852499c245e73ed616c58f27b3b739
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-22 01:43:56 +0200

    Center Window config

--- center_window/center_window.py
+++ center_window/center_window.py
@@ -10,7 +10,7 @@ from tkinter  import messagebox,Tk
 class title:
     def __init__(self, root, font):
         topFrame = tk.Frame(root)
-        topFrame.pack(side=tk.TOP, pady=20)
+        topFrame.pack(side=tk.TOP,pady=20)
         label = tk.Label(topFrame, text="Measure Of Center", font=font + (40, "italic"), fg="#4b50b0", bg="#84e9d9")
         label.pack()
 
@@ -24,11 +24,11 @@ def run(dataFrame):
     root.state('zoomed')
     width= root.winfo_screenwidth()
     height= root.winfo_screenheight()
-    root.title("measures of center")
+    root.title("Measure Of Center")
     font = ("Helvetica",)
     root.option_add('*TCombobox*Listbox.font', font+(15,)) # apply font to combobox
     root.config(bg="#84e9d9")
-    # title(root,font)
+    title(root,font)
     # your code here
     
 
@@ -37,85 +37,110 @@ def run(dataFrame):
     lists=df.columns
     
     frameOptions = tk.Frame(root, bg="#84e9d9")
-    frameOptions.pack(pady=10)
+    frameOptions.pack(side=tk.TOP,pady=40)
     opts=StringVar(root)
     opts.set("Select the Column")
     lists = lists.tolist()
-    men=ttk.Combobox(root,width=30, justify='center', height=len(lists),textvariable=opts,values=lists ,font=font+(16,))
-    men.place(x=350,y=80,width=500)
+    men=ttk.Combobox(frameOptions,width=30, justify='center', height=len(lists),textvariable=opts,values=lists ,font=font+(16,))
     men.option_add('*TCombobox*Listbox.Justify', 'center')
-
+    men.pack()
 
 
     def select(men):
         data=opts.get()
         if(df[opts.get()].dtypes==object):
-           tk.messagebox.showinfo(
-           title='not valid data',
-           message= "invalid data please select valid data"
-           )  
-           return
+            messagebox.showinfo(
+            title='Error!',
+            message= "Invalid data please select valid data"
+            )  
+            return
         return data
     data=select
     def buttonmodefunc():
-        c=stats.mode(df[data])
-        lab1=Label(root,text="mode=",bg="white",fg="black",width="10",height="2")
-        lab1.place(x=650,y=150)
-        l1=Label(root,text=c,bg="white",fg="black",width="20",height="2")
-        l1.place(x=720,y=150)
-
-
+        if(opts.get() == 'Select the Column'):
+            l1.config(text = 'Select a Column')
+        else:
+            c=stats.mode(df[data])
+            l1.config(text = c)
+    
+        
     def buttonmedianfunc():
-        c=stats.median(df[data])
-        lab2=Label(root,text="median=",bg="white",fg="black",width="10",height="2")
-        lab2.place(x=650,y=270)
-        l2=Label(root,text=c,bg="white",fg="black",width="20",height="2")
-        l2.place(x=720,y=270)
-
+        if(opts.get() == 'Select the Column'):
+            l2.config(text = 'Select a Column')
+        else:
+            c=stats.median(df[data])
+            l2.config(text = c)
 
     def buttonmeanfunc():
-        c=stats.mean(df[data])
-        lab3=Label(root,text="mean=",bg="white",fg="black",width="10",height="2")
-        lab3.place(x=650,y=390)
-        l3=Label(root,text=c,bg="white",fg="black",width="20",height="2")
-        l3.place(x=720,y=390)
+        if(opts.get() == 'Select the Column'):
+            l3.config(text = 'Select a Column')
+        else:
+            c=stats.mean(df[data])
+            l3.config(text = c)
 
     def buttonvariancefunc():
-        c=stats.variance(df[data])
-        lab4=Label(root,text="variance=",bg="white",fg="black",width="20",height="2")
-        lab4.place(x=650,y=510)
-        l4=Label(root,text=c,bg="white",fg="black",width="20",height="2")
-        l4.place(x=750,y=510)
+        if(opts.get() == 'Select the Column'):
+            l4.config(text = 'Select a Column')
+        else:
+            c=stats.variance(df[data])
+            l4.config(text = c)
 
     def buttonpvariancefunc():
-        c=stats.pvariance(df[data])
-        lab5=Label(root,text="Stand. Dev.=",bg="white",fg="black",width="20",height="2")
-        lab5.place(x=650,y=630)
-        l5=Label(root,text=c,bg="white",fg="black",width="20",height="2")
-        l5.place(x=760,y=630)    
+        if(opts.get() == 'Select the Column'):
+            l5.config(text = 'Select a Column')
+        else:
+            c=stats.pvariance(df[data])
+            l5.config(text = c) 
 
 
-    # frameBtns1 = tk.Frame(root, bg="#84e9d9")
-    # frameBtns1.pack(pady=0)
-    # button = tk.Button(frameBtns1, text='press to calc mode', command=buttonmodefunc, font=font + (20,), bg="#4b50b0",fg="#efefef",width = 10)
-    # button.pack(side=tk.LEFT,padx = 10,pady = 300)
-    b1=Button(root,text="press to calc mode",command=buttonmodefunc)
-    b1.config(font=("Helvetica",15,"bold"))
-    b1.place(x=360,y=150)
+    frameBtns1 = tk.Frame(root, bg="#84e9d9")
+    frameBtns1.pack(pady=20)
+    button1 = tk.Button(frameBtns1, text='Calculate Mode', command=buttonmodefunc, font=font + (20,), bg="#4b50b0",fg="#efefef",width = 30)
+    l1=Label(frameBtns1,text='', bg="#4b50b0",fg="#efefef",width="20",font=font + (20,))
+    l1.pack(side = tk.RIGHT,padx=20)
+    button1.pack(side = tk.RIGHT)
 
-    b2=Button(root,text="press to calc median",command=buttonmedianfunc)
-    b2.config(font=("Helvetica",15,"bold"))
-    b2.place(x=360,y=270)
 
-    b3=Button(root,text="press to calc mean",command=buttonmeanfunc)
-    b3.config(font=("IHelvetica",15,"bold"))
-    b3.place(x=360,y=390)
 
-    b4=Button(root,text="press to calc variance",command=buttonvariancefunc)
-    b4.config(font=("Helvetica",15,"bold"))
-    b4.place(x=360,y=510)
+    frameBtns2 = tk.Frame(root, bg="#84e9d9")
+    frameBtns2.pack(pady=20)
+    l2=Label(frameBtns2,text='', bg="#4b50b0",fg="#efefef",width="20",font=font + (20,))
+    l2.pack(side = tk.RIGHT,padx=20)
+    button2 = tk.Button(frameBtns2, text='Calculate Median', command=buttonmedianfunc, font=font + (20,), bg="#4b50b0",fg="#efefef",width = 30)
+    button2.pack(side = tk.RIGHT)
 
-    b5=Button(root,text="press to calc Stand. Dev.",command=buttonpvariancefunc)
-    b5.config(font=("Helvetica",15,"bold"))
-    b5.place(x=360,y=630)
+    # b2=Button(root,text="press to calc median",command=buttonmedianfunc)
+    # b2.config(font=("Helvetica",15,"bold"))
+    # b2.place(x=360,y=270)
+    frameBtns3 = tk.Frame(root, bg="#84e9d9")
+    frameBtns3.pack(pady=20)
+    l3=Label(frameBtns3,text='', bg="#4b50b0",fg="#efefef",width="20",font=font + (20,))
+    l3.pack(side = tk.RIGHT,padx=20)
+    button3 = tk.Button(frameBtns3, text='Calculate Mean', command=buttonmeanfunc, font=font + (20,), bg="#4b50b0",fg="#efefef",width = 30)
+    button3.pack(side = tk.RIGHT)
+
+    # b3=Button(root,text="press to calc mean",command=buttonmeanfunc)
+    # b3.config(font=("IHelvetica",15,"bold"))
+    # b3.place(x=360,y=390)
+
+    frameBtns4 = tk.Frame(root, bg="#84e9d9")
+    frameBtns4.pack(pady=20)
+    l4=Label(frameBtns4,text='', bg="#4b50b0",fg="#efefef",width="20",font=font + (20,))
+    l4.pack(side = tk.RIGHT,padx=20)
+    button4 = tk.Button(frameBtns4, text='Calculate Variance', command=buttonvariancefunc, font=font + (20,), bg="#4b50b0",fg="#efefef",width = 30)
+    button4.pack(side = tk.RIGHT)
+    # b4=Button(root,text="press to calc variance",command=buttonvariancefunc)
+    # b4.config(font=("Helvetica",15,"bold"))
+    # b4.place(x=360,y=510)
+
+    frameBtns5 = tk.Frame(root, bg="#84e9d9")
+    frameBtns5.pack(pady=20)
+    l5=Label(frameBtns5,text='', bg="#4b50b0",fg="#efefef",width="20",font=font + (20,))
+    l5.pack(side = tk.RIGHT,padx=20)
+    button5 = tk.Button(frameBtns5, text='Calculate Standard Deviation', command=buttonpvariancefunc, font=font + (20,), bg="#4b50b0",fg="#efefef",width = 30)
+    button5.pack(side = tk.RIGHT)
+
+    # b5=Button(root,text="press to calc Stand. Dev.",command=buttonpvariancefunc)
+    # b5.config(font=("Helvetica",15,"bold"))
+    # b5.place(x=360,y=630)
     root.mainloop()
