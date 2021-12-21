3c549a07193b73e376ca2130435868f724b81861
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-21 21:53:00 +0200

    Some graphs and combobox

--- plot_window/plot_window.py
+++ plot_window/plot_window.py
@@ -1,28 +1,74 @@
 import tkinter as tk
-from tkinter import Listbox, ttk
-from tkinter.constants import CENTER
-from typing import Text
-import matplotlib.pyplot as plt
-import plotly.express as pe
+from tkinter import *
+from tkinter import ttk
 import numpy as np
+import matplotlib.pyplot as plt
+import statistics as stats
+from statistics import *
 import pandas as pd
-# import steamgraphic as sg
-from tkinter import messagebox
-from numpy.lib.function_base import place
+from tkinter  import messagebox,Tk
 options = ['Bar Char', 'Histogram', 'Dot Digram',
            'Box plot', 'Stem-and-leaf Plot']
 
+
 class HistogramBtn:
-    def __init__(self, frame, font,dataFrame):
+    def __init__(self, frame, font,dataFrame,menu1):
         self.dataFrame = dataFrame
+        self.menu1=menu1
         button = tk.Button(frame, text='Histogram', command=self.plot, font=font + (20,), bg="#4b50b0",
                            fg="#efefef",width = 10)
-        button.pack(side=tk.LEFT,padx = 10)
+        button.pack(side=tk.LEFT,padx = 10,pady = 300)
 
     def plot(self):
-        print("hssdas")
-        # self.dataFrame.values.tolist()
+        plt.hist(self.dataFrame[self.menu1.get()],color = 'maroon')
+        plt.show()
         pass
+
+
+class BarGraphBtn:
+    def __init__(self, frame, font,dataFrame,menu1):
+        self.dataFrame = dataFrame
+        self.menu1 = menu1
+        button = tk.Button(frame, text='Bar Chart', command=self.plot, font=font + (20,), bg="#4b50b0",
+                           fg="#efefef",width = 10)
+        button.pack(side=tk.LEFT,padx = 10,pady = 300)
+
+    def plot(self):
+        plt.hist(self.dataFrame[self.menu1.get()],color = 'maroon',rwidth=0.5)
+        plt.show()
+        pass
+
+
+
+class BoxPlotGraphBtn:
+    def __init__(self, frame, font,dataFrame,menu1):
+        self.dataFrame = dataFrame
+        self.menu1 = menu1
+        button = tk.Button(frame, text='Box plot', command=self.plot, font=font + (20,), bg="#4b50b0",
+                           fg="#efefef",width = 10)
+        button.pack(side=tk.LEFT,padx = 10,pady = 300)
+
+    def plot(self):
+        plt.boxplot(self.dataFrame[self.menu1.get()])
+        plt.show()
+        pass
+
+
+class PieGraphBtn:
+    def __init__(self, frame, font,dataFrame,menu1):
+        self.dataFrame = dataFrame
+        self.menu1 = menu1
+        button = tk.Button(frame, text='Pie Chart', command=self.plot, font=font + (20,), bg="#4b50b0",
+                           fg="#efefef",width = 10)
+        button.pack(side=tk.LEFT,padx = 10,pady = 300)
+
+    def plot(self):
+        colors = plt.get_cmap('Blues')(np.linspace(0.2, 0.7))
+        plt.boxplot(self.dataFrame[self.menu1.get()])
+        plt.show()
+        pass
+
+
 
 def run(dataFrame):
     root = tk.Tk()
@@ -35,56 +81,74 @@ def run(dataFrame):
 
     font = ("Helvetica",)
 
+    # get the columns name from the file
+    lists = dataFrame.columns
+    # print(lists)
+    lists=lists.tolist()
+    
+    # Frame for option menu
+    frameOptions = tk.Frame(root, bg="#84e9d9")
+    frameOptions.pack(pady=20)
+
     frameBtns = tk.Frame(root, bg="#84e9d9")
-    frameBtns.pack(pady=30)
+    frameBtns.pack(pady=50)
+    
+    opts=StringVar(frameOptions)
+    opts.set("select the column")
+    # root, width=27, height=3, textvariable=n, values=options
+    men=ttk.Combobox(frameOptions, width=30, height=len(lists),textvariable=opts,values=lists)
+    men.place(x=350,y=80,width=250)
+    men.pack()
+    name = men.get()
+    pltBtnHisto = HistogramBtn(frameBtns, font, dataFrame,men)
+    pltBtnBar = BarGraphBtn(frameBtns, font, dataFrame,men)
+    pltBtnBoxPlot = BoxPlotGraphBtn(frameBtns, font, dataFrame,men)
+    pltBtnpie = PieGraphBtn(frameBtns, font, dataFrame,men)
+    
+    # def showComb():
+    #     n = tk.StringVar()
+    #     global chartchoosen
+    #     chartchoosen = ttk.Combobox(
+    #         root, width=27, height=3, textvariable=n, values=options)
+    #     ttk.Label(root, text="Select Chart:", font=(
+    #         "Helvetica", 10)).place(rely=0.5, relx=0.4, anchor=CENTER)
+    #     chartchoosen.place(rely=0.5, relx=0.5, anchor=CENTER)
+    #     # chartchoosen.pack()
+    #     chartchoosen.current()
+    #     if chartchoosen.get() != "":
+    #         messagebox.showinfo(chartchoosen.get())
 
-    pltBtn = HistogramBtn(frameBtns, font, dataFrame)
-    # Btn2 = HistogramBtn(frameBtns, font, dataFrame)
+    # def menu():
+    #     showComb()
+    #     btn2 = tk.Button(root, text="Get Chart", bg="#4b50b0", fg="#efefef", font=("Helvetica", 10),
+    #                      width=7, command=checkcomb).place(relx=0.6, rely=0.5, anchor=CENTER)
 
-    def showComb():
-        n = tk.StringVar()
-        global chartchoosen
-        chartchoosen = ttk.Combobox(
-            root, width=27, height=3, textvariable=n, values=options)
-        ttk.Label(root, text="Select Chart:", font=(
-            "Helvetica", 10)).place(rely=0.5, relx=0.4, anchor=CENTER)
-        chartchoosen.place(rely=0.5, relx=0.5, anchor=CENTER)
-        # chartchoosen.pack()
-        chartchoosen.current()
-        if chartchoosen.get() != "":
-            messagebox.showinfo(chartchoosen.get())
+    # def checkcomb():
+    #     if chartchoosen.get() == 'Bar Char':
+    #         plt.bar(dataFrame.values.tolist())
+    #         plt.show()
+    #     elif chartchoosen.get() == "Dot Digram":
+    #         plt.plot(dataFrame.values.tolist(), 'ro')
+    #         plt.xlabel('Frequency')
+    #         plt.ylabel('Values')
+    #         plt.show()
+    #     elif chartchoosen.get() == "Box plot":
+    #         plt.boxplot(dataFrame.values.tolist())
+    #         plt.show()
+    #     elif chartchoosen.get() == "Histogram":
+    #         plt.hist(dataFrame.values.tolist())
+    #         plt.show()
+    #     else:
+    #         # sg.stem_graphic(x, scale=10)
+    #         plt.pie(dataFrame.values.tolist())
+    #         plt.show()
 
-    def menu():
-        showComb()
-        btn2 = tk.Button(root, text="Get Chart", bg="#4b50b0", fg="#efefef", font=("Helvetica", 10),
-                         width=7, command=checkcomb).place(relx=0.6, rely=0.5, anchor=CENTER)
-
-    def checkcomb():
-        if chartchoosen.get() == 'Bar Char':
-            plt.bar(dataFrame.values.tolist())
-            plt.show()
-        elif chartchoosen.get() == "Dot Digram":
-            plt.plot(dataFrame.values.tolist(), 'ro')
-            plt.xlabel('Frequency')
-            plt.ylabel('Values')
-            plt.show()
-        elif chartchoosen.get() == "Box plot":
-            plt.boxplot(dataFrame.values.tolist())
-            plt.show()
-        elif chartchoosen.get() == "Histogram":
-            plt.hist(dataFrame.values.tolist())
-            plt.show()
-        else:
-            # sg.stem_graphic(x, scale=10)
-            plt.pie(dataFrame.values.tolist())
-            plt.show()
-
-    # root.tk.title("Charts")
-    # root.configure(bg='#84e9d9')
-    # root.geometry("600x400")
-    btn = tk.Button(root, text="Charts Menu", bg="#4b50b0", fg="#efefef", width=10, height=1,
-                    command=menu).place(relx=0.5, rely=0.4, anchor=CENTER)
-    lbl = tk.Label(root, text="plot")
-    lbl.pack()
+    # # root.tk.title("Charts")
+    # # root.configure(bg='#84e9d9')
+    # # root.geometry("600x400")
+    # btn = tk.Button(root, text="Charts Menu", bg="#4b50b0", fg="#efefef", width=10, height=1,
+    #                 command=menu).place(relx=0.5, rely=0.4, anchor=CENTER)
+    # lbl = tk.Label(root, text="plot")
+    # lbl.pack()
     # print(dataFrame.values.tolist())
     root.mainloop()
