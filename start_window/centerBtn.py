432b27ed5262fee648373584868b54c62242c83a
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-22 02:03:45 +0200

    Buttons names in main screen

--- start_window/centerBtn.py
+++ start_window/centerBtn.py
@@ -12,7 +12,7 @@ import tkinter as tk
 class centerBtn:
     def __init__(self, frame, font,dataFrame):
         self.dataFrame = dataFrame
-        button = tk.Button(frame, text='Measures of center', command=self.plotData, font=font + (20,), bg="#4b50b0",
+        button = tk.Button(frame, text='Measure of center', command=self.plotData, font=font + (20,), bg="#4b50b0",
                            fg="#efefef",width = 15)
         button.pack(side=tk.LEFT,padx = 10)
 
--- start_window/plotBtn.py
+++ start_window/plotBtn.py
@@ -10,7 +10,7 @@ import tkinter as tk
 class plotBtn:
     def __init__(self, frame, font, dataFrame):
         self.dataFrame = dataFrame
-        button = tk.Button(frame, text='Plot', command=self.plotData, font=font + (20,), bg="#4b50b0",
+        button = tk.Button(frame, text='Plot and Graphs', command=self.plotData, font=font + (20,), bg="#4b50b0",
                            fg="#efefef", width=10)
         button.pack(side=tk.LEFT, padx=10)
 
